78c82f35126dc8b309e85803f318244e48d343fe
Hugo Oliveira <hugo.oliveira@al.infnet.edu.br>
2023-12-13 13:18:56 -0300

    Cria os metodos crud no controller

--- src/main/java/at/java/controllers/UsuarioController.java
+++ src/main/java/at/java/controllers/UsuarioController.java
@@ -1,8 +1,27 @@
 package at.java.controllers;
 
+import static spark.Spark.get;
+import static spark.Spark.post;
+import static spark.Spark.put;
+import static spark.Spark.delete;
 public class UsuarioController {
 
     public static void run() {
 
+        post("/usuarios/:id", (request, response) -> {
+            return "";
+        });
+        put("/usuarios/:id", (request, response) -> {
+            return "";
+        });
+        get("/usuarios/:id", (request, response) -> {
+            return "";
+        });
+        get("/usuarios/:id", (request, response) -> {
+            return "";
+        });
+        delete("/usuarios/:id", (request, response) -> {
+            return "";
+        });
     }
 }
