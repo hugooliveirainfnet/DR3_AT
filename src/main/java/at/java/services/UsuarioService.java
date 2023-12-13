9da39fd1c208878fdcf5bb70fc3a971d7da0c9c6
Hugo Oliveira <hugo.oliveira@al.infnet.edu.br>
2023-12-13 11:34:25 -0300

    Cria os metodos crud no usuario service

--- src/main/java/at/java/services/UsuarioService.java
+++ src/main/java/at/java/services/UsuarioService.java
@@ -1,4 +1,49 @@
 package at.java.services;
 
+import at.java.models.Usuario;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Optional;
+
 public class UsuarioService {
+
+    private final List<Usuario> usuarios = new ArrayList<>();
+
+    public void inserir(Usuario usuario) {
+        if(!usuarios.contains(usuario))
+            usuarios.add(usuario);
+    }
+    public void alterar(Usuario usuario) {
+        var usuarioExiste = buscar(usuario.getId());
+
+        if(usuarioExiste != null) {
+            for (Usuario usu : usuarios) {
+                if (usu.getId() == usuario.getId()) {
+                    usu.setNome(usuario.getNome());
+                    usu.setSenha(usuario.getSenha());
+                }
+            }
+        }
+    }
+    public Usuario buscar(int id) {
+        Optional<Usuario> usu = usuarios.stream().filter(usuario -> usuario.getId() == id ).findFirst();
+        if(usu.isPresent()) return usu.get();
+
+        return null;
+    }
+    public List<Usuario> listar() {
+        return usuarios;
+    }
+    public void excluir(int id) {
+        Iterator<Usuario> iterator = usuarios.iterator();
+        while (iterator.hasNext()) {
+            Usuario usuario = iterator.next();
+            if (usuario.getId() == id) {
+                iterator.remove();
+                return;
+            }
+        }
+    }
 }
