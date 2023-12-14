f585ba6aa5827f386921d4b529caf93c3d1c1bff
Hugo Oliveira <hugo.oliveira@al.infnet.edu.br>
2023-12-13 21:47:38 -0300

    Altera a porta padrão do servidor do spark para 8080

--- src/main/java/at/java/Main.java
+++ src/main/java/at/java/Main.java
@@ -1,10 +1,12 @@
 package at.java;
 
 import at.java.controllers.UsuarioController;
+import spark.Spark;
 
 public class Main {
 
     public static void main(String[] args) {
+        Spark.port(8080);
         UsuarioController.run();
     }
 }
