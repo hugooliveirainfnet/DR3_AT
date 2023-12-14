d86f1f0461cdd27d916baf4a73bf437f438eeb10
Hugo Oliveira <hugo.oliveira@al.infnet.edu.br>
2023-12-13 21:49:41 -0300

    Cria os endpoints da aplicação

--- src/main/java/at/java/controllers/UsuarioController.java
+++ src/main/java/at/java/controllers/UsuarioController.java
@@ -1,26 +1,56 @@
 package at.java.controllers;
 
+import at.java.dtos.UsuarioDTOInput;
+import at.java.dtos.UsuarioDTOOutput;
+import at.java.services.UsuarioService;
+import com.fasterxml.jackson.databind.ObjectMapper;
+
 import static spark.Spark.get;
 import static spark.Spark.post;
 import static spark.Spark.put;
 import static spark.Spark.delete;
 public class UsuarioController {
-
+    private static final UsuarioService service =  UsuarioService.getInstance();
+    private static final ObjectMapper objectMapper = new ObjectMapper();
     public static void run() {
 
-        post("/usuarios/:id", (request, response) -> {
+        post("/usuarios", (request, response) -> {
+            UsuarioDTOInput dtoInput = objectMapper.readValue(request.body(), UsuarioDTOInput.class);
+            service.inserir(dtoInput);
+
+            response.status(201);
             return "";
         });
-        put("/usuarios/:id", (request, response) -> {
+
+        put("/usuarios", (request, response) -> {
+            UsuarioDTOInput dtoInput = objectMapper.readValue(request.body(), UsuarioDTOInput.class);
+            service.alterar(dtoInput);
+
+            response.status(204);
             return "";
         });
+
         get("/usuarios/:id", (request, response) -> {
-            return "";
+            Long id = Long.valueOf(request.params("id"));
+            UsuarioDTOOutput dtoOutput = service.buscar(id);
+            String responseJson = objectMapper.writeValueAsString(dtoOutput);
+
+            response.status(200);
+            return responseJson;
         });
-        get("/usuarios/:id", (request, response) -> {
-            return "";
+
+        get("/usuarios", (request, response) -> {
+            String responseJson = objectMapper.writeValueAsString(service.listar());
+
+            response.status(200);
+            return responseJson;
         });
+
         delete("/usuarios/:id", (request, response) -> {
+            Long id = Long.valueOf(request.params("id"));
+            service.excluir(id);
+
+            response.status(204);
             return "";
         });
     }
